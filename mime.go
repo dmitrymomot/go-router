694abfd046373e89d5e73b71aa0de4644aed22df
Dmytro Momot <mail@dmomot.com>
2026-08-27 13:24:23 +0200

    feat(router): add header and media type constants

---
+++ mime.go
@@ -0,0 +1,42 @@
+package router
+
+// Header names that the router and the middleware set or read.
+const (
+	HeaderAccept             = "Accept"
+	HeaderAllow              = "Allow"
+	HeaderAuthorization      = "Authorization"
+	HeaderContentDisposition = "Content-Disposition"
+	HeaderContentLength      = "Content-Length"
+	HeaderContentType        = "Content-Type"
+	HeaderLocation           = "Location"
+	HeaderRetryAfter         = "Retry-After"
+	HeaderVary               = "Vary"
+	HeaderXForwardedFor      = "X-Forwarded-For"
+	HeaderXForwardedProto    = "X-Forwarded-Proto"
+	HeaderXRealIP            = "X-Real-IP"
+	HeaderXRequestID         = "X-Request-Id"
+
+	HeaderAccessControlAllowCredentials = "Access-Control-Allow-Credentials"
+	HeaderAccessControlAllowHeaders     = "Access-Control-Allow-Headers"
+	HeaderAccessControlAllowMethods     = "Access-Control-Allow-Methods"
+	HeaderAccessControlAllowOrigin      = "Access-Control-Allow-Origin"
+	HeaderAccessControlExposeHeaders    = "Access-Control-Expose-Headers"
+	HeaderAccessControlMaxAge           = "Access-Control-Max-Age"
+	HeaderAccessControlRequestHeaders   = "Access-Control-Request-Headers"
+	HeaderAccessControlRequestMethod    = "Access-Control-Request-Method"
+	HeaderOrigin                        = "Origin"
+)
+
+// Media types that the render helpers write.
+const (
+	MIMEApplicationJSON            = "application/json"
+	MIMEApplicationJSONCharsetUTF8 = "application/json; charset=utf-8"
+	MIMEApplicationForm            = "application/x-www-form-urlencoded"
+	MIMEMultipartForm              = "multipart/form-data"
+	MIMEOctetStream                = "application/octet-stream"
+	MIMETextHTML                   = "text/html"
+	MIMETextHTMLCharsetUTF8        = "text/html; charset=utf-8"
+	MIMETextPlain                  = "text/plain"
+	MIMETextPlainCharsetUTF8       = "text/plain; charset=utf-8"
+	MIMETextEventStream            = "text/event-stream"
+)
